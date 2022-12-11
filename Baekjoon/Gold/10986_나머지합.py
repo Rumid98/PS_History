b44d816bc2d5a5baedf177232d78442c6f1f871d
Rumid <bout123456@naver.com>
2022-12-11 22:50:38 +0900

    22.12.11

---
+++ Baekjoon/Gold/10986_나머지합.py
@@ -0,0 +1,21 @@
+from sys import stdin, stdout
+input = stdin.readline
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+answer = 0
+
+
+sums = [0] * (n+1)  # 합 배열
+info = [0] * m  # 나머지 배열
+for i in range(1, n+1):  # 합 배열 저장
+    sums[i] = sums[i-1] + nums[i-1]
+for i in range(1, n+1):  # 합 배열을 m으로 나눈 나머지 값으로 update
+    sums[i] %= m
+for i in range(1, n+1):  # 0 ~ m까지 나머지의 개수 count
+    info[sums[i] % m] += 1
+for i in range(m):
+    if info[i] == 1:
+        answer += 1
+    elif info[i] >= 2:
+        answer += int((info[i]) * (info[i]-1) / 2)  # nC2 계산
+stdout.write(str(answer+info[0]))
