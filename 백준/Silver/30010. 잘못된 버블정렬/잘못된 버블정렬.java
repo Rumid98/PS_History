d24d8759b6215099db6bea806834341576252d6d
Jin-hwa Hong <90546366+Rumid98@users.noreply.github.com>
2024-10-08 12:58:50 +0900

    [Silver V] Title: 잘못된 버블정렬, Time: 80 ms, Memory: 12408 KB -BaekjoonHub

---
+++ 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Scanner scn = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i=n; i>0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
