2f9cd287fdf80d17b9a6c9e2714b8b002df805c2
Jin-hwa Hong <90546366+Rumid98@users.noreply.github.com>
2024-11-15 15:58:27 +0900

    [Gold V] Title: 집합의 표현, Time: 268 ms, Memory: 76584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,36 +1,27 @@
 import sys
 sys.setrecursionlimit(10**6)
-def input(): return sys.stdin.readline().strip()
+input = sys.stdin.readline
 
 
-def main():
-    n, m = map(int, input().split())
-    parent = [i for i in range(n+1)]
+def union(a, b):
+    a_parent, b_parent = find(a), find(b)
+    if a_parent <= b_parent:
+        nodes[b_parent] = a_parent
+    else:
+        nodes[a_parent] = b_parent
 
-    def union(x, y):
-        root_x = find(x)
-        root_y = find(y)
-        if root_x <= root_y:
-            parent[root_y] = root_x
-        else:
-            parent[root_x] = root_y
+def find(v):
+    if v == nodes[v]:
+        return v
+    else:
+        nodes[v] = find(nodes[v])
+        return nodes[v]
 
-    def find(x):
-        if x == parent[x]:
-            return x
-        parent[x] = find(parent[x])
-        return parent[x]
-
-    for _ in range(m):
-        command = list(map(int, input().split()))
-        if command[0] == 0:  # Union
-            union(command[1], command[2])
-        else:  # Find
-            if find(command[1]) == find(command[2]):
-                print('YES')
-            else:
-                print('NO')
-
-
-if __name__ == "__main__":
-    main()
+n, m = map(int, input().split())
+nodes = [i for i in range(n+1)]
+for _ in range(m):
+    command, a, b = map(int, input().split())
+    if command == 0:
+        union(a, b)
+    else:
+        print('YES') if find(a) == find(b) else print('NO')
